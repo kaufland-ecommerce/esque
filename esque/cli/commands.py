3a81f4e2a05e550d6d809c0cf9d9c1b1c294dec3
Kevin Kreitner <kevin.kreitner@real-digital.de>
2019-06-11 13:59:38 +0200

    Fix ping command

--- esque/cli/commands.py
+++ esque/cli/commands.py
@@ -271,7 +271,7 @@ def ping(state, times, wait):
     try:
         try:
             topic_controller.create_topics(
-                [topic_controller.get_topic(PING_TOPIC, state.cluster)]
+                [topic_controller.get_topic(PING_TOPIC)]
             )
         except TopicAlreadyExistsException:
             click.echo("Topic already exists.")
@@ -290,7 +290,7 @@ def ping(state, times, wait):
     except KeyboardInterrupt:
         pass
     finally:
-        topic_controller.delete_topic(PING_TOPIC)
+        topic_controller.delete_topic(topic_controller.get_topic(PING_TOPIC))
         click.echo("--- statistics ---")
         click.echo(f"{len(deltas)} messages sent/received")
         click.echo(
