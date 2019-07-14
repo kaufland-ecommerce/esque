85052e842aeb48de6bc91b2a7769d47426dbf344
Kevin Kreitner <kevin.kreitner@real-digital.de>
2019-07-14 21:05:35 +0200

    Add tests and update ci config

--- tests/integration/test_clients.py
+++ tests/integration/test_clients.py
@@ -3,13 +3,13 @@ import pathlib
 import pytest
 
 from esque.avromessage import AvroFileReader
-from esque.clients import FileConsumer, AvroFileConsumer
+from esque.clients import FileConsumer, AvroFileConsumer, FileProducer, AvroFileProducer
 from esque.message import PlainTextFileReader
 from esque.topic import Topic
 
 
 @pytest.mark.integration
-def test_plaint_text_consume_to_file(consumer_group, filled_topic: Topic, working_dir: pathlib.Path):
+def test_plain_text_consume_to_file(consumer_group, filled_topic: Topic, working_dir: pathlib.Path):
     file_consumer = FileConsumer(consumer_group, filled_topic.name, working_dir, False)
     number_of_consumer_messages = file_consumer.consume(10)
 
@@ -36,3 +36,27 @@ def test_avro_consume_to_file(consumer_group, filled_avro_topic: Topic, working_
 
     assert number_of_consumer_messages == 10
     assert len(messages) == 10
+
+
+@pytest.mark.integration
+def test_plain_text_consume_and_produce(consumer_group, filled_topic: Topic, topic: str, working_dir: pathlib.Path):
+    file_consumer = FileConsumer(consumer_group, filled_topic.name, working_dir, False)
+    number_of_consumer_messages = file_consumer.consume(10)
+
+    producer = FileProducer(working_dir)
+    number_of_produced_messages = producer.produce(topic)
+
+    assert number_of_consumer_messages == 10
+    assert number_of_produced_messages == 10
+
+
+@pytest.mark.integration
+def test_avro_consume_and_produce(consumer_group, filled_avro_topic: Topic, topic: str, working_dir: pathlib.Path):
+    file_consumer = AvroFileConsumer(consumer_group, filled_avro_topic.name, working_dir, False)
+    number_of_consumer_messages = file_consumer.consume(10)
+
+    producer = AvroFileProducer(working_dir)
+    number_of_produced_messages = producer.produce(topic)
+
+    assert number_of_consumer_messages == 10
+    assert number_of_produced_messages == 10
