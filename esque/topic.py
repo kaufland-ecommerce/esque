8eb78ce5089716f9ca7eadc096f9ae138475a955
MrTrustworthy <MrTrustworthy@users.noreply.github.com>
2019-09-12 14:45:59 +0200

    Find a consistent, non-surprising way to handle replication factor and partition amount for new and existing topics (#44)
    * diffing now done differently, also includes partitions and replicas
    
    * removed clients from topic object
    
    * black fix
    
    * diff logic now makes sense
    
    * stupid CI
    
    * same test env in ci and local
    
    * some improvements to avoid topic auto creation
    
    * fuckfuckfuckthisshit
    
    * Modifies the docker-stack to be more similar to travis
    
    * Move stack
    
    * no sleep
    
    * Gives credit to wurstmeister
    
    * Fixed black.

--- esque/topic.py
+++ esque/topic.py
@@ -1,11 +1,10 @@
 from collections import namedtuple
 from functools import total_ordering
-from typing import Dict, List, Union, Optional
+from typing import Dict, List, Union, Optional, Tuple, Any, Generator
 
 import yaml
 from pykafka.protocol.offset import OffsetPartitionResponse
 
-from esque.errors import raise_for_kafka_exception
 from esque.resource import KafkaResource
 
 TopicDict = Dict[str, Union[int, str, Dict[str, str]]]
@@ -22,7 +21,7 @@ class Partition(KafkaResource):
         high_watermark: int,
         partition_isrs,
         partition_leader,
-        partition_replicas,
+        partition_replicas: List[int],  # list of brokers holding a replica
     ):
         self.partition_id = partition_id
         self.watermark = Watermark(high_watermark, low_watermark)
@@ -41,6 +40,87 @@ class Partition(KafkaResource):
         }
 
 
+class AttributeDiff:
+    def __init__(self, remote, local):
+        self.remote = remote
+        self.local = local
+        assert type(remote) == type(
+            local
+        ), f"Attributes should be given as the same type, not {type(remote)} and {type(local)}"
+
+    @property
+    def old(self):
+        return self.remote
+
+    @property
+    def new(self):
+        return self.local
+
+    def __eq__(self, other: "AttributeDiff"):
+        return self.remote == other.remote and self.local == other.local
+
+    def __hash__(self):
+        return hash(hash(self.remote) + hash(self.local))
+
+    def __repr__(self):
+        return f"<AttributeDiff[remote:{self.remote}, local:{self.local}]>"
+
+
+class TopicDiff:
+    INVALID_CHANGES = ["num_partitions", "replication_factor"]
+
+    def __init__(self):
+        self._diffs: Dict[str, AttributeDiff] = {}
+
+    def set_diff(self, name: str, remote, local) -> "TopicDiff":
+
+        # config values of local topics can be set as int or string
+        # since all "config" variables we get from cluster topics are sent to us as strings,
+        # we need to convert the local ones to string to match them correctly (exclude None's)
+        local = str(local) if isinstance(remote, str) and local is not None else local
+
+        if remote == local:
+            return self
+
+        # TODO: this should be handled correctly by checking the cluster defaults, like
+        # if remote == cluster_default(name) and local is None: return
+        # currently, if an attribute that was set get's un-set, it's ignored
+        if local is None:
+            return self
+
+        assert type(remote) == type(
+            local
+        ), f"Attributes for {name} should be given as the same type, not {type(remote)} and {type(local)}"
+        self._diffs[name] = AttributeDiff(remote, local)
+
+        # allow chaining of set-calls
+        return self
+
+    @property
+    def is_valid(self) -> bool:
+        return set(self._diffs.get(a, None) for a in self.INVALID_CHANGES) == {None}
+
+    @classmethod
+    def from_dict(cls, diff_dict: Dict[str, AttributeDiff]) -> "TopicDiff":
+        td = TopicDiff()
+        td._diffs = diff_dict
+        return td
+
+    @property
+    def has_changes(self) -> bool:
+        return len(self._diffs.keys()) > 0
+
+    def changes(self) -> Generator[Tuple[str, Any, Any], None, None]:
+        for key, val in self._diffs.items():
+            yield key, val.remote, val.local
+
+    def __eq__(self, other: "TopicDiff") -> bool:
+        return self._diffs == other._diffs
+
+    def __repr__(self):
+        return f"<TopicDiff[{str(self._diffs)}>"
+
+
 @total_ordering
 class Topic(KafkaResource):
     def __init__(
@@ -55,33 +135,20 @@ class Topic(KafkaResource):
             name = name.decode("ascii")
         self.name = name
 
-        # TODO remove those two, replace with the properties below
-        self.num_partitions = num_partitions
-        self.replication_factor = replication_factor
+        # those settings are only used until the topic is updated from cluster
+        self.__num_partitions = num_partitions
+        self.__replication_factor = replication_factor
+
         self.config = config if config is not None else {}
 
-        self._partitions: Optional[List[Partition]] = None
-        self._pykafka_topic = None
-        self._confluent_topic = None
-
+        self.partition_data: Optional[List[Partition]] = None
         self.is_only_local = True
 
     # properties
     @property
     def partitions(self) -> List[Partition]:
         assert not self.is_only_local, "Need to update topic before updating partitions"
-        return self._partitions
-
-    @property
-    def partition_amount(self) -> int:
-        return len(self.partitions)
-
-    @property
-    def replication(self) -> int:
-        reps = set(p.partition_replicas for p in self.partitions)
-        if len(reps) != 1:
-            raise ValueError(f"Topic partitions have different replication factors! {reps}")
-        return reps.pop()
+        return self.partition_data
 
     @property
     def offsets(self) -> Dict[int, Watermark]:
@@ -89,6 +156,20 @@ class Topic(KafkaResource):
         Returns the low and high watermark for each partition in a topic
         """
         return {partition.partition_id: partition.watermark for partition in self.partitions}
+
+    @property
+    def num_partitions(self) -> int:
+        if self.is_only_local:
+            return self.__num_partitions
+        return len(self.partitions)
+
+    @property
+    def replication_factor(self) -> int:
+        if self.is_only_local:
+            return self.__replication_factor
+        partition_replication_factors = set(len(p.partition_replicas) for p in self.partitions)
+        assert len(partition_replication_factors) == 1, "Different replication factors for partitions!"
+        return partition_replication_factors.pop()
 
     # conversions and factories
     @classmethod
@@ -116,25 +197,6 @@ class Topic(KafkaResource):
         new_values = yaml.safe_load(data)
         for attr, value in new_values.items():
             setattr(self, attr, value)
-
-    # update hook (TODO move to topic controller/factory?)
-    @raise_for_kafka_exception
-    def update_partitions(self, low_watermarks: PartitionInfo, high_watermarks: PartitionInfo):
-
-        partitions = []
-        for t in self._confluent_topic.values():
-            for partition_id, partition_meta in t.partitions.items():
-                partition = Partition(
-                    partition_id,
-                    int(low_watermarks[partition_id].offset[0]),
-                    int(high_watermarks[partition_id].offset[0]),
-                    partition_meta.isrs,
-                    partition_meta.leader,
-                    partition_meta.replicas,
-                )
-                partitions.append(partition)
-
-        self._partitions = partitions
 
     # object behaviour
     def __lt__(self, other: "Topic"):
--- esque/topic_controller.py
+++ esque/topic_controller.py
@@ -1,25 +1,47 @@
 import re
-from collections import namedtuple
-from typing import List, Dict, TYPE_CHECKING
+from enum import Enum
+from typing import List, TYPE_CHECKING, Union
 
-from confluent_kafka.admin import ConfigResource
+from confluent_kafka.admin import ConfigResource, TopicMetadata as ConfluentTopic
 from confluent_kafka.cimpl import NewTopic
+from pykafka.topic import Topic as PyKafkaTopic
 
 from esque.config import Config
 from esque.errors import raise_for_kafka_exception
 from esque.helpers import invalidate_cache_after, ensure_kafka_futures_done
-from esque.topic import Topic
+from esque.topic import Topic, PartitionInfo, Partition, TopicDiff
 
 if TYPE_CHECKING:
     from esque.cluster import Cluster
 
-AttributeDiff = namedtuple("AttributeDiff", ["old", "new"])
+
+class ClientTypes(Enum):
+    Confluent = "Confluent"
+    PyKafka = "PyKafka"
+
+
+ClientType = Union[ConfluentTopic, PyKafkaTopic]
 
 
 class TopicController:
     def __init__(self, cluster: "Cluster", config: Config):
         self.cluster: "Cluster" = cluster
         self.config = config
+
+    @raise_for_kafka_exception
+    def _get_client_topic(self, topic_name: str, client_type: ClientTypes) -> ClientType:
+        confluent_topics = self.cluster.confluent_client.list_topics(topic=topic_name, timeout=10).topics
+        if client_type == ClientTypes.Confluent:
+            return confluent_topics[topic_name]
+        elif client_type == ClientTypes.PyKafka:
+            # at least PyKafka does it's own caching, so we don't have to bother
+            pykafka_topics = self.cluster.pykafka_client.cluster.topics
+            # PyKafka will try to auto-create the topic if you use `topic[random_name]`
+            if topic_name not in confluent_topics.keys():
+                raise KeyError(f"Topic {topic_name} does not exist")
+            return pykafka_topics[topic_name]
+        else:
+            raise ValueError(f"TopicType needs to be part of {ClientTypes}")
 
     @raise_for_kafka_exception
     def list_topics(self, *, search_string: str = None, sort: bool = True, hide_internal: bool = True) -> List[Topic]:
@@ -73,27 +95,45 @@ class TopicController:
     @raise_for_kafka_exception
     def update_from_cluster(self, topic: Topic):
         """Takes a topic and, based on its name, updates all attributes from the cluster"""
-        if topic.is_only_local:  # only have to instantiate those once
-            topic._pykafka_topic = self.cluster.pykafka_client.cluster.topics[topic.name]
-            topic._confluent_topic = self.cluster.confluent_client.list_topics(topic=topic.name, timeout=10).topics
 
-        # TODO put the topic instances into a cache of this class
-        low_watermarks = topic._pykafka_topic.earliest_available_offsets()
-        high_watermarks = topic._pykafka_topic.latest_available_offsets()
-        topic.update_partitions(low_watermarks, high_watermarks)
+        confluent_topic: ConfluentTopic = self._get_client_topic(topic.name, ClientTypes.Confluent)
+        pykafka_topic: PyKafkaTopic = self._get_client_topic(topic.name, ClientTypes.PyKafka)
 
+        low_watermarks = pykafka_topic.earliest_available_offsets()
+        high_watermarks = pykafka_topic.latest_available_offsets()
+
+        topic.partition_data = self._get_partition_data(confluent_topic, low_watermarks, high_watermarks)
         topic.config = self.cluster.retrieve_config(ConfigResource.Type.TOPIC, topic.name)
+
         topic.is_only_local = False
+
         return topic
 
     @raise_for_kafka_exception
-    def diff_with_cluster(self, topic: Topic) -> Dict[str, AttributeDiff]:
-        cluster_state = self.cluster.retrieve_config(ConfigResource.Type.TOPIC, topic.name)
-        out = {}
-        for name, old_value in cluster_state.items():
-            new_val = topic.config.get(name)
-            if not new_val or str(new_val) == str(old_value):
-                continue
-            out[name] = AttributeDiff(str(old_value), str(new_val))
+    def _get_partition_data(
+        self, confluent_topic: ConfluentTopic, low_watermarks: PartitionInfo, high_watermarks: PartitionInfo
+    ) -> List[Partition]:
 
-        return out
+        partitions = []
+
+        for partition_id, meta in confluent_topic.partitions.items():
+            low = int(low_watermarks[partition_id].offset[0])
+            high = int(high_watermarks[partition_id].offset[0])
+            partition = Partition(partition_id, low, high, meta.isrs, meta.leader, meta.replicas)
+            partitions.append(partition)
+
+        return partitions
+
+    @raise_for_kafka_exception
+    def diff_with_cluster(self, local_topic: Topic) -> TopicDiff:
+        assert local_topic.is_only_local, "Can only diff local topics with remote"
+
+        cluster_topic = self.get_cluster_topic(local_topic.name)
+        diffs = TopicDiff()
+        diffs.set_diff("num_partitions", cluster_topic.num_partitions, local_topic.num_partitions)
+        diffs.set_diff("replication_factor", cluster_topic.replication_factor, local_topic.replication_factor)
+
+        for name, old_value in cluster_topic.config.items():
+            diffs.set_diff(name, old_value, local_topic.config.get(name))
+
+        return diffs
