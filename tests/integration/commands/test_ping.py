17fa3bba1725b5bc1228edf9d710955e168ffea4
Jannik Hoffjann <jannik.hoffjann@real-digital.de>
2019-09-19 16:16:31 +0200

    Fixed ping test

--- tests/integration/commands/test_ping.py
+++ tests/integration/commands/test_ping.py
@@ -1,4 +1,3 @@
-import confluent_kafka
 import pytest
 from click.testing import CliRunner
 
@@ -15,14 +14,13 @@ def test_smoke_test_ping(cli_runner: CliRunner):
 
 
 @pytest.mark.integration
-def test_correct_amount_of_messages(
-    mocker, cli_runner: CliRunner, confluent_admin_client: confluent_kafka.admin.AdminClient, topic_id: str
-):
-    config.RANDOM = "test"
-
+def test_correct_amount_of_messages(mocker, cli_runner: CliRunner, topic_controller: TopicController):
     topic_controller_delete_topic = mocker.patch.object(TopicController, "delete_topic", mocker.Mock())
 
     result = cli_runner.invoke(ping)
 
     assert result.exit_code == 0
     assert topic_controller_delete_topic.call_count == 1
+
+    ping_topic = topic_controller.get_cluster_topic(config.PING_TOPIC)
+    assert ping_topic.offsets[0].high == 10
