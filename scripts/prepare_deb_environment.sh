f1735fce16cd17069bd7a0cffaa9ba63a534aeb2
ognjen-j <ognjen.joldzic@real-digital.de>
2019-09-25 17:06:30 +0200

    [WIP] - path fixes

--- scripts/prepare_deb_environment.sh
+++ scripts/prepare_deb_environment.sh
@@ -8,25 +8,27 @@ PROJECT_DESCRIPTION=$4
 DEB_STAGING_PATH=$5
 SOURCE_CODE_PATH=$6
 VERSION="1.0"   # default, if there is no __version__.py for some reason
+# we should be in the package root folder
+cd ./$SOURCE_CODE_PATH
 if [ -f ../esque/__version__.py ]
 then
-  VERSION=`cat ../esque/__version__.py | cut -f2 -d= | sed 's/ //g' | sed 's/"//g'`
+  VERSION=`cat ./esque/__version__.py | cut -f2 -d= | sed 's/ //g' | sed 's/"//g'`
 fi
 # just in case
 rm -rf $1
 # create a new staging environment
 FULL_PACKAGE_ROOT=${DEB_STAGING_PATH}/${NAME}-${VERSION}/DEBIAN
 mkdir --parent ${FULL_PACKAGE_ROOT}/src
-cp ${SOURCE_CODE_PATH}/installation/deb/* ${FULL_PACKAGE_ROOT}/
-sed -i 's/__LIBRARY__/'${NAME}'/g' ${FULL_PACKAGE_ROOT}/control
-sed -i 's/__MAINTAINER__/'${PROJECT_MAINTAINER}'/g' ${FULL_PACKAGE_ROOT}/control
-sed -i 's/__VERSION__/'${VERSION}'/g' ${FULL_PACKAGE_ROOT}/control
-sed -i 's/__HOMEPAGE__/'${PROJECT_HOMEPAGE}'/g' ${FULL_PACKAGE_ROOT}/control
-sed -i 's/__DESCRIPTION__/'${PROJECT_DESCRIPTION}'/g' ${FULL_PACKAGE_ROOT}/control
-cp ${SOURCE_CODE_PATH}/* ${FULL_PACKAGE_ROOT}/src/
-cp -r ${SOURCE_CODE_PATH}/scripts ${FULL_PACKAGE_ROOT}/src/
-cp -r ${SOURCE_CODE_PATH}/esque ${FULL_PACKAGE_ROOT}/src/
-cp -r ${SOURCE_CODE_PATH}/tests ${FULL_PACKAGE_ROOT}/src/
+cp ./installation/deb/* ${FULL_PACKAGE_ROOT}/
+sed -i 's:__LIBRARY__:'${NAME}':g' ${FULL_PACKAGE_ROOT}/control
+sed -i 's:__MAINTAINER__:'${PROJECT_MAINTAINER}':g' ${FULL_PACKAGE_ROOT}/control
+sed -i 's:__VERSION__:'${VERSION}':g' ${FULL_PACKAGE_ROOT}/control
+sed -i 's:__HOMEPAGE__:'${PROJECT_HOMEPAGE}':g' ${FULL_PACKAGE_ROOT}/control
+sed -i 's:__DESCRIPTION__:'${PROJECT_DESCRIPTION}':g' ${FULL_PACKAGE_ROOT}/control
+cp ./* ${FULL_PACKAGE_ROOT}/src/
+cp -r ./scripts ${FULL_PACKAGE_ROOT}/src/
+cp -r ./esque ${FULL_PACKAGE_ROOT}/src/
+cp -r ./tests ${FULL_PACKAGE_ROOT}/src/
 # build the package
 cd ${DEB_STAGING_PATH}
 dpkg-deb --build ${NAME}-${VERSION}
