bf423be78d2d4cc617d902f10b4e5c198a7f959c
ognjen-j <ognjen.joldzic@real-digital.de>
2019-09-25 17:08:47 +0200

    [WIP] - path test

--- scripts/prepare_deb_environment.sh
+++ scripts/prepare_deb_environment.sh
@@ -9,7 +9,9 @@ DEB_STAGING_PATH=$5
 SOURCE_CODE_PATH=$6
 VERSION="1.0"   # default, if there is no __version__.py for some reason
 # we should be in the package root folder
-cd ./$SOURCE_CODE_PATH
+echo "Source code path: ${SOURCE_CODE_PATH}, current dir: `pwd`"
+exit 0
+cd ./${SOURCE_CODE_PATH}
 if [ -f ../esque/__version__.py ]
 then
   VERSION=`cat ./esque/__version__.py | cut -f2 -d= | sed 's/ //g' | sed 's/"//g'`
