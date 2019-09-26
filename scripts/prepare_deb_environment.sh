87d40509e31eb22cd3a5c9e144805c72bd62335f
Ognjen Joldžić <joldzic@propelmedia.com>
2019-09-26 22:04:19 +0200

    [WIP] - simplified the deb building procedure

--- scripts/prepare_deb_environment.sh
+++ scripts/prepare_deb_environment.sh
@@ -17,19 +17,13 @@ fi
 # just in case
 rm -rf ${DEB_STAGING_PATH}
 # create a new staging environment
-FULL_PACKAGE_ROOT=${DEB_STAGING_PATH}/${NAME}-${VERSION}/DEBIAN
-mkdir --parent ${FULL_PACKAGE_ROOT}/src
-cp ./installation/deb/* ${FULL_PACKAGE_ROOT}/
-sed -i 's,__LIBRARY__,'"${NAME}"',g' ${FULL_PACKAGE_ROOT}/control
-sed -i 's,__MAINTAINER__,'"${PROJECT_MAINTAINER}"',g' ${FULL_PACKAGE_ROOT}/control
-sed -i 's,__VERSION__,'"${VERSION}"',g' ${FULL_PACKAGE_ROOT}/control
-sed -i 's,__HOMEPAGE__,'"${PROJECT_HOMEPAGE}"',g' ${FULL_PACKAGE_ROOT}/control
-sed -i 's,__DESCRIPTION__,'"${PROJECT_DESCRIPTION}"',g' ${FULL_PACKAGE_ROOT}/control
-cp ./* ${FULL_PACKAGE_ROOT}/src/ 2>/dev/null
-cp -r ./scripts ${FULL_PACKAGE_ROOT}/src/
-cp -r ./esque ${FULL_PACKAGE_ROOT}/src/
-cp -r ./tests ${FULL_PACKAGE_ROOT}/src/
-# build the package
-cd ${DEB_STAGING_PATH}
-dpkg-deb --build ${NAME}-${VERSION}
-# we would have to send the packaged binary to a repository somewhere
+CONTROL_DIRECTORY=${DEB_STAGING_PATH}/DEBIAN
+mkdir --parent ${CONTROL_DIRECTORY}
+cp ./installation/deb/* ${CONTROL_DIRECTORY}/
+sed -i 's,__LIBRARY__,'"${NAME}"',g' ${CONTROL_DIRECTORY}/control
+sed -i 's,__MAINTAINER__,'"${PROJECT_MAINTAINER}"',g' ${CONTROL_DIRECTORY}/control
+sed -i 's,__VERSION__,'"${VERSION}"',g' ${CONTROL_DIRECTORY}/control
+sed -i 's,__HOMEPAGE__,'"${PROJECT_HOMEPAGE}"',g' ${CONTROL_DIRECTORY}/control
+sed -i 's,__DESCRIPTION__,'"${PROJECT_DESCRIPTION}"',g' ${CONTROL_DIRECTORY}/control
+cp ${SOURCE_CODE_PATH}/* ${DEB_STAGING_PATH}/ 2>/dev/null
+
