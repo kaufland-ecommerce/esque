2f2f7d463aca6281eab29e06b22dceb9b540351e
Ognjen Joldžić <joldzic@propelmedia.com>
2019-09-27 00:27:17 +0200

    [WIP] - bugfix

--- scripts/prepare_rpm_environment.sh
+++ scripts/prepare_rpm_environment.sh
@@ -17,19 +17,19 @@ then
   VERSION=`cat ./esque/__version__.py | cut -f2 -d= | sed 's/ //g' | sed 's/"//g'`
 fi
 cp -r ${SOURCE_CODE_PATH}/* ${RPM_STAGING_PATH}/
-echo "%pre" >> ${RPM_STAGING_PATH}/.spec
+echo "%pre" >> ${RPM_STAGING_PATH}/${NAME}.spec
 cat ${SOURCE_CODE_PATH}/installation/rpm/instenv >> ${RPM_STAGING_PATH}/.spec
 cat ${SOURCE_CODE_PATH}/installation/rpm/preinst >> ${RPM_STAGING_PATH}/.spec
 echo "" >> ${RPM_STAGING_PATH}/.spec
-echo "%post" >> ${RPM_STAGING_PATH}/.spec
+echo "%post" >> ${RPM_STAGING_PATH}/${NAME}.spec
 cat ${SOURCE_CODE_PATH}/installation/rpm/instenv >> ${RPM_STAGING_PATH}/.spec
 cat ${SOURCE_CODE_PATH}/installation/rpm/postinst >> ${RPM_STAGING_PATH}/.spec
 echo "" >> ${RPM_STAGING_PATH}/.spec
-echo "%preun" >> ${RPM_STAGING_PATH}/.spec
+echo "%preun" >> ${RPM_STAGING_PATH}/${NAME}.spec
 cat ${SOURCE_CODE_PATH}/installation/rpm/instenv >> ${RPM_STAGING_PATH}/.spec
 cat ${SOURCE_CODE_PATH}/installation/rpm/prerm >> ${RPM_STAGING_PATH}/.spec
 echo "" >> ${RPM_STAGING_PATH}/.spec
-echo "%postun" >> ${RPM_STAGING_PATH}/.spec
+echo "%postun" >> ${RPM_STAGING_PATH}/${NAME}.spec
 cat ${SOURCE_CODE_PATH}/installation/rpm/instenv >> ${RPM_STAGING_PATH}/.spec
 cat ${SOURCE_CODE_PATH}/installation/rpm/postrm >> ${RPM_STAGING_PATH}/.spec
 exit 0
