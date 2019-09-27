083df2d233a805fa30c23b3ec08f0c86eae2e280
ognjen-j <ognjen.joldzic@real-digital.de>
2019-09-27 16:50:42 +0200

    [WIP] - bugfix

--- scripts/deb_package_and_build.sh
+++ scripts/deb_package_and_build.sh
@@ -5,4 +5,9 @@ python3 setup.py sdist --formats=gztar
 archive_name=`ls dist | head -1`
 py2dsc --with-python2=False --with-python3=True --dist-dir=./deb_final/ dist/${archive_name}
 # build the actual package
+dest_folder_name=`echo ${archive_name} | sed 's/.tar.gz//g'`
+cd deb_final/${deb_folder_name}
 dpkg-buildpackage -rfakeroot -uc -us
+# the resulting deb package is one directory up, in deb_final
+cd ..
+deb_package=`ls *.deb | head -1`
