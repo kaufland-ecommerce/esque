1df2b92f900827dd2475548f4961ff2b35772a70
ognjen-j <ognjen.joldzic@real-digital.de>
2019-09-27 16:07:02 +0200

    [WIP] - added the py build process directly as a step in the workflow

---
+++ scripts/deb_package_and_build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export PYBUILD_DISABLE=test/python3
+python3 setup.py --formats=gztar
+# the built archive is in dist, by default
+archive_name=`ls dist | head -1`
+py2dsc --with-python2=False --with-python3=True --dist-dir=./deb_final/ dist/${archive_name}
+# build the actual package
+dpkg-buildpackage -rfakeroot -uc -us
