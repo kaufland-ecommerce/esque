79e9ae4abd9de3bafd27ffdea26bdcea17af7512
Kevin Kreitner <kevin.kreitner@real-digital.de>
2019-09-27 08:42:12 +0200

    Use verbose mode for apply command tests

--- tests/integration/commands/test_apply.py
+++ tests/integration/commands/test_apply.py
@@ -29,7 +29,7 @@ def test_apply(cli_runner, topic_controller: TopicController, topic_id: str):
 
     # 1: topic creation
     path = save_yaml(topic_id, apply_conf)
-    result = cli_runner.invoke(apply, ["-f", path], input="Y\n")
+    result = cli_runner.invoke(apply, ["-f", path, "--verbose"], input="Y\n")
     assert (
         result.exit_code == 0 and "Successfully applied changes" in result.output
     ), f"Calling apply failed, error: {result.output}"
@@ -61,7 +61,7 @@ def test_apply(cli_runner, topic_controller: TopicController, topic_id: str):
     topic_1["num_partitions"] = 3
     topic_1["config"]["cleanup.policy"] = "delete"
     path = save_yaml(topic_id, apply_conf)
-    result = cli_runner.invoke(apply, ["-f", path], input="Y\n")
+    result = cli_runner.invoke(apply, ["-f", path, "--verbose"], input="Y\n")
     assert (
         result.exit_code == 0 and "to `replication_factor` and `num_partitions`" in result.output
     ), f"Calling apply failed, error: {result.output}"
@@ -90,7 +90,7 @@ def test_apply_duplicate_names(cli_runner: CliRunner, topic_id: str):
 
     # having the same topic name twice in apply should raise an ValueError
     path = save_yaml(topic_id, apply_conf)
-    result = cli_runner.invoke(apply, ["-f", path], input="Y\n")
+    result = cli_runner.invoke(apply, ["-f", path, "--verbose"], input="Y\n")
     assert result.exit_code != 0 and isinstance(result.exception, ValueError), f"Calling apply should have failed"
 
 
