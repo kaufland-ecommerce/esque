c7b3adba377881c7b39277ed8114cba7ea64b28a
ognjen-j <ognjen.joldzic@real-digital.de>
2019-09-27 16:35:10 +0200

    [WIP] - bugfix

--- scripts/deb_package_and_build.sh
+++ scripts/deb_package_and_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PYBUILD_DISABLE=test/python3
-python3 setup.py --formats=gztar
+python3 setup.py sdist --formats=gztar
 # the built archive is in dist, by default
 archive_name=`ls dist | head -1`
 py2dsc --with-python2=False --with-python3=True --dist-dir=./deb_final/ dist/${archive_name}
