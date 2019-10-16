ef030d8c15737ae05c30999276b815ab161f2a55
Swen Wenzel <swen.wenzel@real-digital.de>
2019-10-16 14:14:46 +0200

    fix issues with sasl and ssl configuration

--- esque/config/__init__.py
+++ esque/config/__init__.py
@@ -1,10 +1,12 @@
 import configparser
 import random
 import string
+import logging
 from pathlib import Path
 from typing import Any, Dict, List, Optional
 
 import click
+from pykafka import SslConfig
 from pykafka.sasl_authenticators import BaseAuthenticator, ScramAuthenticator, PlainAuthenticator
 
 from esque.cli.environment import ESQUE_CONF_PATH
@@ -13,6 +15,7 @@ from esque.errors import (
     ContextNotDefinedException,
     MissingSaslParameter,
     UnsupportedSaslMechanism,
+    ConfigException
 )
 
 RANDOM = "".join(random.choices(string.ascii_lowercase, k=8))
@@ -20,6 +23,7 @@ PING_TOPIC = f"ping-{RANDOM}"
 PING_GROUP_ID = f"ping-{RANDOM}"
 SLEEP_INTERVAL = 2
 SUPPORTED_SASL_MECHANISMS = ("PLAIN", "SCRAM-SHA-256", "SCRAM-SHA-512")
+log = logging.getLogger(__name__)
 
 
 def config_dir() -> Path:
@@ -58,8 +62,15 @@ class Config:
 
     @property
     def current_context_dict(self) -> Dict[str, Any]:
+        return self._get_section_dict(self._current_section)
+
+    def _get_section_dict(self, section) -> Dict[str, str]:
+        if not self._cfg.has_section(section):
+            log.debug(f"Section {section} not found in config.")
+            return {}
+
         return {
-            option: self._cfg.get(self._current_section, option) for option in self._cfg.options(self._current_section)
+            option: self._cfg.get(section, option) for option in self._cfg.options(section)
         }
 
     @property
@@ -110,12 +121,34 @@ class Config:
         return self.sasl_params["mechanism"] if self.sasl_params else None
 
     @property
-    def sasl_params(self) -> Optional[Dict[str, str]]:
-        return self.current_context_dict.get("sasl", None)
+    def sasl_params(self) -> Dict[str, str]:
+        return self._get_section_dict(f"{self._current_section}.sasl")
+
+    @property
+    def ssl_params(self) -> Optional[Dict[str, str]]:
+        section = f"{self._current_section}.ssl"
+        if not self._cfg.has_section(section):
+            return None
+        return self._get_section_dict(section)
 
     @property
     def security_protocol(self) -> str:
-        return self.current_context_dict["security_protocol"]
+        protocol = self.current_context_dict.get("security_protocol", "PLAINTEXT")
+        if 'SASL' in protocol and self.sasl_mechanism is None:
+            msg = (
+                f"Security protocol {protocol} contains 'SASL' indicating that you want to connect to "
+                "a SASL enabled endpoint but you didn't specify a sasl mechanism.\n"
+                f"Run `esque edit config` and add `sasl_mechanism` to section '[{self._current_section}.sasl]'"
+            )
+            raise ConfigException(msg)
+        if 'SSL' in protocol and self.ssl_params is None:
+            msg = (
+                f"Security protocol {protocol} contains 'SSL' indicating that you want to connect to "
+                "an SSL enabled endpoint but you didn't specify any ssl settings.\n"
+                f"Run `esque edit config` and add at least an empty section '[{self._current_section}.ssl]'"
+            )
+            raise ConfigException(msg)
+        return protocol
 
     def context_switch(self, context: str):
         click.echo(f"Switched to context: {context}")
@@ -128,17 +161,28 @@ class Config:
         with config_path().open("w") as f:
             self._cfg.write(f)
 
-    def create_pykafka_config(self) -> Dict[str, str]:
-        return {"hosts": ",".join(self.bootstrap_servers), "sasl_authenticator": self.get_pykafka_authenticator()}
+    def create_pykafka_config(self) -> Dict[str, Any]:
+        config = {"hosts": ",".join(self.bootstrap_servers)}
+        if self.sasl_mechanism is not None:
+            config["sasl_authenticator"] = self._get_pykafka_authenticator()
+        if self.ssl_params is not None:
+            config["ssl_config"] = self._get_pykafka_ssl_conf()
+        log.debug(f"Created pykafka config: {config}")
+        return config
 
     def create_confluent_config(self, *, debug: bool = False) -> Dict[str, str]:
 
-        base_config = {"bootstrap.servers": ",".join(self.bootstrap_servers), "security.protocol": "PLAINTEXT"}
+        base_config = {
+            "bootstrap.servers": ",".join(self.bootstrap_servers),
+            "security.protocol": self.security_protocol,
+        }
         config = base_config.copy()
         if debug:
             config.update({"debug": "all", "log_level": "2"})
 
         config.update(self._get_confluent_sasl_config())
+        config.update(self._get_confluent_ssl_config())
+        log.debug(f"Created confluent config: {config}")
         return config
 
     def _get_confluent_sasl_config(self) -> Dict[str, str]:
@@ -150,7 +194,6 @@ class Config:
                     "sasl.mechanisms": self.sasl_mechanism,
                     "sasl.username": self.sasl_params["user"],
                     "sasl.password": self.sasl_params["password"],
-                    "security.protocol": self.security_protocol,
                 }
             except KeyError as e:
                 raise MissingSaslParameter(f"SASL mechanism {self.sasl_mechanism} requires parameter {e.args[0]}")
@@ -160,7 +203,28 @@ class Config:
                 f"Supported meachnisms are {SUPPORTED_SASL_MECHANISMS}."
             )
 
-    def get_pykafka_authenticator(self) -> BaseAuthenticator:
+    def _get_confluent_ssl_config(self) -> Dict[str, str]:
+        ssl_params = self.ssl_params
+        rdk_conf = {}
+        if ssl_params is not None:
+            if ssl_params.get("cafile"):
+                rdk_conf["ssl.ca.location"] = ssl_params["cafile"]
+            if ssl_params.get("certfile"):
+                rdk_conf["ssl.certificate.location"] = ssl_params["certfile"]
+            if ssl_params.get("keyfile"):
+                rdk_conf["ssl.key.location"] = ssl_params["keyfile"]
+            if ssl_params.get("password"):
+                rdk_conf["ssl.key.password"] = ssl_params["password"]
+        return rdk_conf
+
+    def _get_pykafka_ssl_conf(self) -> Optional[SslConfig]:
+        ssl_params = self.ssl_params
+        if ssl_params is None:
+            return None
+        ssl_params.setdefault("cafile", None)
+        return SslConfig(**ssl_params)
+
+    def _get_pykafka_authenticator(self) -> BaseAuthenticator:
         try:
             if self.sasl_mechanism == "PLAIN":
                 return PlainAuthenticator(
