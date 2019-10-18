71d4aea2a4b27409897bcbdc0cedd5a03dbb8773
Jannik Hoffjann <jannik.hoffjann@real-digital.de>
2019-10-18 15:00:43 +0200

    Fix test

--- tests/integration/commands/test_describe.py
+++ tests/integration/commands/test_describe.py
@@ -12,10 +12,12 @@ def test_smoke_test_describe_topic(cli_runner: CliRunner, topic: str):
 
 
 @pytest.mark.integration
-def test_describe_topic_consumergroup_in_output(cli_runner: CliRunner, filled_topic: str, partly_read_consumer_group):
+def test_describe_topic_consumergroup_in_output(
+    cli_runner: CliRunner, filled_topic: str, partly_read_consumer_group: str
+):
     result = cli_runner.invoke(describe_topic, [filled_topic, "-C"])
 
     assert result.exit_code == 0
     # TODO: Would like to be able to read this as yaml. We should implement json and yaml outputs
     # output = yaml.safe_load(result.output)
-    assert result.output.contains("partly_read_consumer_group")
+    assert result.output.contains(partly_read_consumer_group)
