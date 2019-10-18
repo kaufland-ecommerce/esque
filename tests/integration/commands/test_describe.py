7be71063529c15d5125922187a736c6c161f6348
Jannik Hoffjann <jannik.hoffjann@real-digital.de>
2019-10-18 14:59:41 +0200

    Add integration tests for consumergroup loading

--- tests/integration/commands/test_describe.py
+++ tests/integration/commands/test_describe.py
@@ -9,3 +9,13 @@ def test_smoke_test_describe_topic(cli_runner: CliRunner, topic: str):
     result = cli_runner.invoke(describe_topic, [topic])
 
     assert result.exit_code == 0
+
+
+@pytest.mark.integration
+def test_describe_topic_consumergroup_in_output(cli_runner: CliRunner, filled_topic: str, partly_read_consumer_group):
+    result = cli_runner.invoke(describe_topic, [filled_topic, "-C"])
+
+    assert result.exit_code == 0
+    # TODO: Would like to be able to read this as yaml. We should implement json and yaml outputs
+    # output = yaml.safe_load(result.output)
+    assert result.output.contains("partly_read_consumer_group")
