0805cf1f86158cfcab07eca16afd7a35b1b1709b
Garrett Thomas <garrettcarrollthomas@gmail.com>
2019-10-21 14:57:48 +0200

    62 accept and output standardized stdin and stdout (#81)
    * Is actually working, just need to get the tests working!
    
    * First tests written
    
    * now mocking click.getchar
    
    * describe topic saves information in ordered dict
    
    * check_if_confirmation_possible function and start formatting output
    
    * finish standardized output options and add tests
    
    * Fix some tests
    
    * Fix edit topic name
    
    * reformat with black
    
    * check which key is not in local kafka
    
    * fix test_describe
    
    * add tests to test if commands fail when missing a required argument
    
    * format with black
    
    * fix some of swens comments
    
    * add types to yaml representers, test file for output.py
    
    * interactive and non-interactive cli runners
    
    * move check_if_confirmation_possible to ensure_approval
    
    * add print statements to test to see why they fail in travis ci
    
    * print output to see if 'no config ...' printed
    
    * make code fail to see output
    
    * input no and print ctx result
    
    * purposely fail two more tests and print output
    
    * check if confluent_admin_client somehow creates the config file
    
    * only run tests of interest
    
    * correct test command
    
    * test with test_config fixture in non_interactive_cli_runner
    
    * add test_config fixture in cli runner fixtures
    
    * remove session scope, which cause a problem with mock
    
    * create click_stdin object to enable patching
    
    * dont use click_stdin to read line
    
    * fix flake8 errors
    
    * fix flake8 errors
    
    * comments fixed
    
    * refactor exceptions
    
    * refactor error handler, enable test debugging
    
    * change get_offsets topic-name to option
    
    * fix flake8 errors
    
    * black reformatting
    
    * error_handler decorator on edit topic
    
    * fix (final) set of comments!!!!!!
    
    * Update tests/integration/commands/test_get.py
    
    Co-Authored-By: Swen Wenzel <5111028+swenzel@users.noreply.github.com>
    
    * fix __version__ import statement

--- tests/integration/commands/test_creation.py
+++ tests/integration/commands/test_creation.py
@@ -1,24 +1,85 @@
 import confluent_kafka
 import pytest
+from click import MissingParameter
 from click.testing import CliRunner
 
 from esque.cli.commands import create_topic
 from esque.cli.options import State
+from esque.errors import NoConfirmationPossibleException
 from esque.resources.topic import Topic
 
 
 @pytest.mark.integration
-def test_create(cli_runner: CliRunner, confluent_admin_client: confluent_kafka.admin.AdminClient, topic_id: str):
-
-    cli_runner.invoke(create_topic, [topic_id])
+def test_create_without_confirmation_does_not_create_topic(
+    interactive_cli_runner: CliRunner, confluent_admin_client: confluent_kafka.admin.AdminClient, topic_id: str
+):
+    result = interactive_cli_runner.invoke(create_topic, [topic_id])
+    assert result.exit_code == 0
 
     topics = confluent_admin_client.list_topics(timeout=5).topics.keys()
     assert topic_id not in topics
 
 
 @pytest.mark.integration
+def test_create_topic_without_topic_name_fails(non_interactive_cli_runner: CliRunner):
+    result = non_interactive_cli_runner.invoke(create_topic)
+    assert result.exit_code == MissingParameter.exit_code
+
+
+@pytest.mark.integration
+def test_create_topic_as_argument_with_verification_works(
+    interactive_cli_runner: CliRunner, confluent_admin_client: confluent_kafka.admin.AdminClient, topic_id: str
+):
+
+    topics = confluent_admin_client.list_topics(timeout=5).topics.keys()
+    assert topic_id not in topics
+
+    result = interactive_cli_runner.invoke(create_topic, args=topic_id, input="Y\n")
+    assert result.exit_code == 0
+    # invalidate cache
+    confluent_admin_client.poll(timeout=1)
+    topics = confluent_admin_client.list_topics(timeout=5).topics.keys()
+    assert topic_id in topics
+
+
+@pytest.mark.integration
+def test_create_topic_with_stdin_works(
+    non_interactive_cli_runner: CliRunner, confluent_admin_client: confluent_kafka.admin.AdminClient, topic_id: str
+):
+
+    topics = confluent_admin_client.list_topics(timeout=5).topics.keys()
+    assert topic_id not in topics
+
+    result = non_interactive_cli_runner.invoke(create_topic, args="--no-verify", input=topic_id)
+    assert result.exit_code == 0
+    # invalidate cache
+    confluent_admin_client.poll(timeout=1)
+    topics = confluent_admin_client.list_topics(timeout=5).topics.keys()
+    assert topic_id in topics
+
+
+@pytest.mark.integration
+def test_topic_creation_stops_in_non_interactive_mode_without_no_verify(
+    non_interactive_cli_runner: CliRunner, confluent_admin_client: confluent_kafka.admin.AdminClient, topic_id: str
+):
+    topics = confluent_admin_client.list_topics(timeout=5).topics.keys()
+    assert topic_id not in topics
+
+    result = non_interactive_cli_runner.invoke(create_topic, input=topic_id)
+    assert result.exit_code == NoConfirmationPossibleException.exit_code
+
+    # Invalidate cache
+    confluent_admin_client.poll(timeout=1)
+    topics = confluent_admin_client.list_topics(timeout=5).topics.keys()
+    assert topic_id not in topics
+
+
+@pytest.mark.integration
 def test_topic_creation_with_template_works(
-    state: State, confluent_admin_client: confluent_kafka.admin.AdminClient, topic_id: str
+    non_interactive_cli_runner: CliRunner,
+    state: State,
+    confluent_admin_client: confluent_kafka.admin.AdminClient,
+    topic_id: str,
 ):
     topic_1 = topic_id + "_1"
     topic_2 = topic_id + "_2"
@@ -36,8 +97,8 @@ def test_topic_creation_with_template_works(
     state.cluster.topic_controller.create_topics(
         [Topic(topic_1, replication_factor=replication_factor, num_partitions=num_partitions, config=config)]
     )
-    runner = CliRunner()
-    runner.invoke(create_topic, ["--no-verify", "-l", topic_1, topic_2])
+    result = non_interactive_cli_runner.invoke(create_topic, ["--no-verify", "-l", topic_1, topic_2])
+    assert result.exit_code == 0
     config_from_template = state.cluster.topic_controller.get_cluster_topic(topic_2)
     assert config_from_template.replication_factor == replication_factor
     assert config_from_template.num_partitions == num_partitions
