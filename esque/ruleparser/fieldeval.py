ed2fbe588a588ae753d55ac2aeffcb73f7498eff
ognjen-j <ognjen.joldzic@real-digital.de>
2019-11-04 13:07:44 +0100

    Simplified for flake8 reasons.

--- esque/ruleparser/fieldeval.py
+++ esque/ruleparser/fieldeval.py
@@ -27,47 +27,45 @@ class FieldEval:
             return datetime.datetime.strftime(datetime.datetime.now(), self.__time_format)
         elif self.__message is None:
             return -1
-        elif field_name == Operator.FIELDS["MESSAGE_OFFSET"].replace("\\", ""):
-            if isinstance(self.__message, Message):
-                return self.__message.offset()
-            elif isinstance(self.__message, KafkaMessage):
-                return -1
+        elif isinstance(self.__message, Message):
+            return self.__evaluate_message_field(field_name=field_name)
+        elif isinstance(self.__message, KafkaMessage):
+            return self.__evaluate_kafka_message_field(field_name=field_name)
+
+    def __evaluate_message_field(self, field_name: str):
+        if field_name == Operator.FIELDS["MESSAGE_OFFSET"].replace("\\", ""):
+            return self.__message.offset()
         elif field_name == Operator.FIELDS["MESSAGE_PARTITION"].replace("\\", ""):
-            if isinstance(self.__message, Message):
-                partition = self.__message.partition()
-            elif isinstance(self.__message, KafkaMessage):
-                partition = self.__message.partition
+            partition = self.__message.partition()
             return -1 if not partition else partition
-        elif field_name == Operator.FIELDS["MESSAGE_TIMESTAMP"].replace("\\", "") and isinstance(
-            self.__message, Message
-        ):
+        elif field_name == Operator.FIELDS["MESSAGE_TIMESTAMP"].replace("\\", ""):
             return datetime.datetime.strftime(
                 datetime.datetime.fromtimestamp(self.__message.timestamp()[1] / 1000.0), self.__time_format
             )
         elif field_name == Operator.FIELDS["MESSAGE_LENGTH"].replace("\\", ""):
-            if isinstance(self.__message, Message):
-                return len(self.__message)
-            elif isinstance(self.__message, KafkaMessage):
-                return len(self.__message.key) + len(self.__message.value)
+            return len(self.__message)
         elif field_name == Operator.FIELDS["MESSAGE_KEY"].replace("\\", ""):
-            if isinstance(self.__message, Message):
-                return self.__message.key().decode("UTF-8")
-            elif isinstance(self.__message, KafkaMessage):
-                return self.__message.key
-        elif self.__header_pattern.fullmatch(field_name) and isinstance(self.__message, Message):
+            return self.__message.key().decode("UTF-8")
+        elif self.__header_pattern.fullmatch(field_name):
             header_name = field_name.split(".")[-1]
-            if isinstance(self.__message, Message):
-                if self.__message.headers() is None:
-                    return ""
-                decoded_message = decode_message(message=self.__message)
-                for message_header in decoded_message.headers:
-                    if message_header.key == header_name:
-                        return message_header.value if message_header.value else ""
-            elif isinstance(self.__message, KafkaMessage):
-                for message_header in self.__message.headers:
-                    if message_header.key == header_name and message_header.value:
-                        return message_header.value
-                    elif message_header.key == header_name:
-                        return ""
-                return ""
+            if self.__message.headers() is not None:
+                message_headers = decode_message(message=self.__message).headers
+                return next((message_header.value for message_header in message_headers if message_header.key == header_name), "no_header")
+            return "no_header"
+        return -1
+
+    def __evaluate_kafka_message_field(self, field_name: str):
+        if field_name == Operator.FIELDS["MESSAGE_OFFSET"].replace("\\", ""):
+            return -1
+        elif field_name == Operator.FIELDS["MESSAGE_PARTITION"].replace("\\", ""):
+            partition = self.__message.partition
+            return -1 if not partition else partition
+        elif field_name == Operator.FIELDS["MESSAGE_LENGTH"].replace("\\", ""):
+            return len(self.__message.key) + len(self.__message.value)
+        elif field_name == Operator.FIELDS["MESSAGE_KEY"].replace("\\", ""):
+            return self.__message.key
+        elif self.__header_pattern.fullmatch(field_name):
+            header_name = field_name.split(".")[-1]
+            message_headers = self.__message.headers
+            return next((message_header.value for message_header in message_headers if message_header.key == header_name), "no_header")
         return -1
