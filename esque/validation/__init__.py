66b54310069727dfd540bd4b68f4064b836edd53
Swen Wenzel <swen.wenzel@real-digital.de>
2019-11-05 13:06:30 +0100

    change validation logic and create extra validator for edit topic

--- esque/validation/__init__.py
+++ esque/validation/__init__.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Dict
 
 import yamale
 
@@ -8,13 +9,17 @@ from . import validators
 SCHEMA_DIR = Path(__file__).resolve().parent / "schemas"
 
 
-def validate_topic_config(topic_config: Path) -> None:
+def validate_topic_config(topic_config: Dict) -> None:
     validate(topic_config, SCHEMA_DIR / "topic.yaml")
 
 
-def validate(file: Path, schema: Path) -> None:
+def validate_editable_topic_config(editable_topic_config: Dict) -> None:
+    validate(editable_topic_config, SCHEMA_DIR / "editable_topic.yaml")
+
+
+def validate(data: Dict, schema: Path) -> None:
     schema = yamale.make_schema(schema, validators=validators.all_validators())
     try:
-        yamale.validate(schema, file, strict=True)
+        yamale.validate(schema, [(data, "<dict literal>")], strict=True)
     except ValueError as validation_error:
         raise TopicConfigNotValidException(validation_error)
