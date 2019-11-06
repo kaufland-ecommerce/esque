7ad78d550f67f8ab6ef5a8d86d83662b8f73c9d0
Swen Wenzel <swen.wenzel@real-digital.de>
2019-11-06 08:24:43 +0100

    update Config to support new yaml schema

--- esque/config/__init__.py
+++ esque/config/__init__.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import Any, Dict, List, Optional
 
 import click
+import yaml
 
 from esque.cli.environment import ESQUE_CONF_PATH
 from esque.config.migration import get_config_version, check_config_version
@@ -43,56 +44,27 @@ def sample_config_path() -> Path:
 
 class Config:
     def __init__(self):
-        self._cfg = configparser.ConfigParser()
-        self._current_dict: Optional[Dict[str, str]] = None
-        if config_path().exists():
-            check_config_version(config_path())
-            self._cfg.read(config_path())
-        else:
+        if not config_path().exists():
             raise ConfigNotExistsException()
+        check_config_version(config_path())
+        self._cfg = yaml.safe_load(config_path().read_text())
+        self._current_dict: Optional[Dict[str, str]] = None
 
     @property
-    def available_contexts(self):
-        return sorted([key.split(".")[1] for key in self._cfg.keys() if key.startswith("Context.")])
+    def available_contexts(self) -> List[str]:
+        return sorted(self._cfg["contexts"])
 
     @property
-    def current_context(self):
-        return self._cfg.get("Context", "current")
+    def default_values(self) -> Dict[str, Any]:
+        return self.current_context_dict["default_values"]
 
     @property
-    def _current_section(self):
-        return f"Context.{self._cfg.get('Context', 'current')}"
+    def current_context(self) -> str:
+        return self._cfg["current_context"]
 
     @property
     def current_context_dict(self) -> Dict[str, Any]:
-        if self._current_dict is None:
-            self._current_dict = self._get_section_dict(self._current_section)
-        return self._current_dict
-
-    def _get_section_dict(self, section) -> Dict[str, str]:
-        if not self._cfg.has_section(section):
-            log.debug(f"Section {section} not found in config.")
-            return {}
-
-        return {option: self._cfg.get(section, option) for option in self._cfg.options(section)}
-
-    @property
-    def bootstrap_port(self) -> str:
-        if "bootstrap_port" in self.current_context_dict.keys():
-            return self.current_context_dict["bootstrap_port"]
-        return "9092"
-
-    @property
-    def bootstrap_domain(self) -> Optional[str]:
-        config_dict = self.current_context_dict
-        if "bootstrap_domain" in config_dict:
-            return config_dict["bootstrap_domain"]
-        return None
-
-    @property
-    def bootstrap_hosts(self) -> List[str]:
-        config_dict = self.current_context_dict
-        return config_dict["bootstrap_hosts"].split(",")
+        return self._cfg["contexts"][self.current_context]
 
     @property
     def schema_registry(self) -> str:
@@ -100,15 +72,12 @@ class Config:
         return config_dict["schema_registry"]
 
     @property
-    def bootstrap_servers(self):
-        if self.bootstrap_domain:
-            return [f"{host_name}.{self.bootstrap_domain}:{self.bootstrap_port}" for host_name in self.bootstrap_hosts]
-        return [f"{host_name}:{self.bootstrap_port}" for host_name in self.bootstrap_hosts]
+    def bootstrap_servers(self) -> str:
+        return ",".join(self.current_context_dict["bootstrap_servers"])
 
     @property
     def default_partitions(self) -> int:
-        config_dict = self.current_context_dict
-        return int(config_dict["default_partitions"])
+        return self.default_values["num_partitions"]
 
     @property
     def default_replication_factor(self) -> int:
@@ -126,17 +95,25 @@ class Config:
         return "SASL" in self.security_protocol
 
     @property
+    def sasl_params(self) -> Dict[str, str]:
+        return self.current_context_dict.get("sasl_params", {})
+
+    @property
+    def ssl_params(self) -> Dict[str, str]:
+        return self.current_context_dict.get("ssl_params", {})
+
+    @property
     def ssl_enabled(self) -> bool:
         return "SSL" in self.security_protocol
 
     @property
     def security_protocol(self) -> str:
         protocol = self.current_context_dict.get("security_protocol", "PLAINTEXT")
-        if "SASL" in protocol and "sasl_mechanism" not in self.current_context_dict:
+        if "SASL" in protocol and "sasl_params" not in self.current_context_dict:
             msg = (
                 f"Security protocol {protocol} contains 'SASL' indicating that you want to connect to "
-                "a SASL enabled endpoint but you didn't specify a sasl mechanism.\n"
-                f"Run `esque edit config` and add `sasl_mechanism` to section '[{self._current_section}]'.\n"
+                "a SASL enabled endpoint but you didn't specify sasl parameters.\n"
+                f"Run `esque config edit` and add `sasl_params` to section 'contexts.{self.current_context}'.\n"
                 f"Valid mechanisms are {SUPPORTED_SASL_MECHANISMS}."
             )
             raise ConfigException(msg)
@@ -145,13 +122,12 @@ class Config:
     def context_switch(self, context: str):
         if context not in self.available_contexts:
             raise ContextNotDefinedException(f"{context} not defined in {config_path()}")
-        self._update_config("Context", "current", context)
-        self._current_dict = None
+        self._cfg["current_context"] = context
+        self._dump_config()
 
-    def _update_config(self, section: str, key: str, value: str):
-        self._cfg.set(section, key, value=value)
+    def _dump_config(self):
         with config_path().open("w") as f:
-            self._cfg.write(f)
+            yaml.dump(self._cfg, f, default_flow_style=False, sort_keys=False, Dumper=yaml.SafeDumper)
 
     def create_pykafka_config(self) -> Dict[str, Any]:
         config = {"hosts": ",".join(self.bootstrap_servers)}
@@ -183,12 +159,12 @@ class Config:
             try:
                 return {
                     "sasl.mechanisms": self.sasl_mechanism,
-                    "sasl.username": self.current_context_dict["sasl_user"],
-                    "sasl.password": self.current_context_dict["sasl_password"],
+                    "sasl.username": self.sasl_params["user"],
+                    "sasl.password": self.sasl_params["password"],
                 }
             except KeyError as e:
                 msg = f"SASL mechanism {self.sasl_mechanism} requires parameter {e.args[0]}.\n"
-                msg += f"Please run `esque edit config` and add it to section [{self.current_context}]."
+                msg += f"Please run `esque config edit` and add it to section [{self.current_context}]."
                 raise MissingSaslParameter(msg)
         else:
             raise UnsupportedSaslMechanism(
@@ -201,13 +177,13 @@ class Config:
             return {}
         rdk_conf = {}
         if self.current_context_dict.get("ssl_cafile"):
-            rdk_conf["ssl.ca.location"] = self.current_context_dict["ssl_cafile"]
+            rdk_conf["ssl.ca.location"] = self.ssl_params["cafile"]
         if self.current_context_dict.get("ssl_certfile"):
-            rdk_conf["ssl.certificate.location"] = self.current_context_dict["ssl_certfile"]
+            rdk_conf["ssl.certificate.location"] = self.ssl_params["certfile"]
         if self.current_context_dict.get("ssl_keyfile"):
-            rdk_conf["ssl.key.location"] = self.current_context_dict["ssl_keyfile"]
+            rdk_conf["ssl.key.location"] = self.ssl_params["keyfile"]
         if self.current_context_dict.get("ssl_password"):
-            rdk_conf["ssl.key.password"] = self.current_context_dict["ssl_password"]
+            rdk_conf["ssl.key.password"] = self.ssl_params["password"]
         return rdk_conf
 
     def _get_pykafka_ssl_conf(self) -> Optional[SslConfig]:
@@ -215,26 +191,26 @@ class Config:
             return None
         ssl_params = {"cafile": self.current_context_dict.get("ssl_cafile", None)}
         if self.current_context_dict.get("ssl_certfile"):
-            ssl_params["certfile"] = self.current_context_dict["ssl_certfile"]
+            ssl_params["certfile"] = self.ssl_params["certfile"]
         if self.current_context_dict.get("ssl_keyfile"):
-            ssl_params["keyfile"] = self.current_context_dict["ssl_keyfile"]
+            ssl_params["keyfile"] = self.ssl_params["keyfile"]
         if self.current_context_dict.get("ssl_password"):
-            ssl_params["password"] = self.current_context_dict["ssl_password"]
+            ssl_params["password"] = self.ssl_params["password"]
         return SslConfig(**ssl_params)
 
     def _get_pykafka_authenticator(self) -> BaseAuthenticator:
         try:
             if self.sasl_mechanism == "PLAIN":
                 return PlainAuthenticator(
-                    user=self.current_context_dict["sasl_user"],
-                    password=self.current_context_dict["sasl_password"],
+                    user=self.sasl_params["user"],
+                    password=self.sasl_params["password"],
                     security_protocol=self.security_protocol,
                 )
             elif self.sasl_mechanism in ("SCRAM-SHA-256", "SCRAM-SHA-512"):
                 return ScramAuthenticator(
                     self.sasl_mechanism,
-                    user=self.current_context_dict["sasl_user"],
-                    password=self.current_context_dict["sasl_password"],
+                    user=self.sasl_params["user"],
+                    password=self.sasl_params["password"],
                     security_protocol=self.security_protocol,
                 )
             else:
@@ -244,5 +220,5 @@ class Config:
                 )
         except KeyError as e:
             msg = f"SASL mechanism {self.sasl_mechanism} requires parameter {e.args[0]}.\n"
-            msg += f"Please run `esque edit config` and add it to section [{self.current_context}]."
+            msg += f"Please run `esque config edit` and add it to section [{self.current_context}]."
             raise MissingSaslParameter(msg)
