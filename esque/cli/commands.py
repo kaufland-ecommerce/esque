a9c8fa2925db005e31a3dcf5dfa449648653c5c6
ognjen-j <ognjen.joldzic@real-digital.de>
2019-11-06 09:41:21 +0100

    Bugfix

--- esque/cli/commands.py
+++ esque/cli/commands.py
@@ -115,20 +115,20 @@ def ctx(state: State, context: str):
         click.echo(f"Switched to context: {context}")
 
 
-@config.command("autocomplete")
+@config.command("autocomplete", help="Generate the autocompletion script.")
 @error_handler
-def configer_autocomplete():
+def config_autocomplete():
     directory = config_dir()
     config_file_name = "autocomplete.sh"
     config_file = Path(directory / config_file_name)
-    current_shell = pwd.getpwnam(getpass.getuser()).pw_shell
+    current_shell = pwd.getpwnam(getpass.getuser()).pw_shell.split("/")[-1]
     source_designator = "source" if current_shell in ["bash", "sh"] else "source_zsh"
     default_environment = ".bashrc" if current_shell in ["bash", "sh"] else ".zshrc"
     with open(config_file.absolute(), "w") as config_fd:
         config_fd.write("_ESQUE_COMPLETE=" + source_designator + " esque")
-    click.echo("Autocompletion script generated to " + green_bold(config_file.absolute()))
+    click.echo("Autocompletion script generated to " + green_bold(str(config_file.absolute())))
     click.echo("To use the autocompletion feature, simply source the contents of the script into your environment, e.g.")
-    click.echo("\t\techo -e \"\\nsource " + config_file.absolute() + "\" >> " + pwd.getpwnam(getpass.getuser()).pw_dir + "/" + default_environment)
+    click.echo("\t\techo -e \"\\nsource " + str(config_file.absolute()) + "\" >> " + str(pwd.getpwnam(getpass.getuser()).pw_dir) + "/" + default_environment)
 
 
 @create.command("topic")
