253ce6fe7f93477237ea29b5949146f72cf78a60
garrettthomas <garrett.thomas@real-digital.de>
2019-11-14 11:51:44 +0100

    fix stuff after merge

--- tests/conftest.py
+++ tests/conftest.py
@@ -94,6 +94,12 @@ def broker_id(state: State) -> str:
 
 
 @pytest.fixture()
+def broker_host_and_port(state: State) -> str:
+    brokers = Broker.get_all(state.cluster)
+    yield "{}:{}".format(brokers[0].host, brokers[0].port)
+
+
+@pytest.fixture()
 def topic_object(cluster: Cluster, topic: str):
     yield cluster.topic_controller.get_cluster_topic(topic)
 
