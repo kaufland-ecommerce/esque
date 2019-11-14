77de7d756f8410f54305036a895fe2ab5e3fb1c1
ognjen-j <ognjen.joldzic@real-digital.de>
2019-11-14 11:36:28 +0100

    Style tests. Fixed the ping test.

--- tests/integration/commands/test_ping.py
+++ tests/integration/commands/test_ping.py
@@ -14,11 +14,11 @@ def test_smoke_test_ping(non_interactive_cli_runner: CliRunner):
 
 @pytest.mark.integration
 def test_correct_amount_of_messages(mocker, non_interactive_cli_runner: CliRunner, topic_controller: TopicController):
-    topic_controller_delete_topic = mocker.patch.object(TopicController, "delete_topic", mocker.Mock())
+    delete_topic_mock = mocker.patch.object(TopicController, "delete_topic", mocker.Mock())
 
     result = non_interactive_cli_runner.invoke(ping, catch_exceptions=False)
     assert result.exit_code == 0
-    assert topic_controller_delete_topic.call_count == 1
+    assert delete_topic_mock.call_count == 1
 
     ping_topic = topic_controller.get_cluster_topic(config.PING_TOPIC)
     assert ping_topic.watermarks[0].high == 10
