adab3e1acc2f25e71e7ee94c292a37cef77de0c2
Swen Wenzel <swen.wenzel@real-digital.de>
2019-11-14 09:06:38 +0100

    add set_instance method to SingletonMixin

--- esque/helpers.py
+++ esque/helpers.py
@@ -19,8 +19,12 @@ class SingletonMixin(Generic[T]):
     @classmethod
     def get_instance(cls) -> T:
         if cls.__instance is None:
-            cls.__instance = cls()
+            cls.set_instance(cls())
         return cls.__instance
+
+    @classmethod
+    def set_instance(cls, instance: T):
+        cls.__instance = instance
 
 
 def invalidate_cache_before(func):
