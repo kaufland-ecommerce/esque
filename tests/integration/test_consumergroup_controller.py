71cd00938ff62d456dbfcdc78a5bf10dd033eed7
ognjen-j <ognjen.joldzic@real-digital.de>
2020-10-19 12:23:38 +0200

    Test modification.

--- tests/integration/test_consumergroup_controller.py
+++ tests/integration/test_consumergroup_controller.py
@@ -19,7 +19,7 @@ def test_list_consumer_groups(partly_read_consumer_group: str, consumergroup_con
 def test_delete_consumer_groups(partly_read_consumer_group: str, consumergroup_controller: ConsumerGroupController):
     groups_before_deletion = consumergroup_controller.list_consumer_groups()
     assert partly_read_consumer_group in groups_before_deletion
-    consumergroup_controller.delete_consumergroups(consumer_ids=[partly_read_consumer_group])
+    consumergroup_controller.delete_consumer_groups(consumer_ids=[partly_read_consumer_group])
     groups_after_deletion = consumergroup_controller.list_consumer_groups()
     assert partly_read_consumer_group not in groups_after_deletion
 
@@ -27,6 +27,7 @@ def test_delete_consumer_groups(partly_read_consumer_group: str, consumergroup_c
 def test_delete_nonexistent_consumer_groups(
     partly_read_consumer_group: str, consumergroup_controller: ConsumerGroupController
 ):
-    consumergroup_controller.delete_consumergroups(consumer_ids=["definitely_nonexistent"])
-    groups = consumergroup_controller.list_consumer_groups()
-    assert partly_read_consumer_group in groups
+    groups_before = consumergroup_controller.list_consumer_groups()
+    consumergroup_controller.delete_consumer_groups(consumer_ids=["definitely_nonexistent"])
+    groups_after = consumergroup_controller.list_consumer_groups()
+    assert groups_before == groups_after
