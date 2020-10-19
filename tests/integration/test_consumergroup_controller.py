5dde9b320bce49c5ba9dc9b6dc8d5efacb952659
Ognjen Joldzic <44299700+ognjen-j@users.noreply.github.com>
2020-10-19 12:41:52 +0200

    Integrated the new kafka-python and added delete consumergroup command. (#162)
    * Integrated the new kafka-python and added delete consumergroup command.
    
    * Slightly modified the test.
    
    * Consistency fixes.
    
    * Test modification.

--- tests/integration/test_consumergroup_controller.py
+++ tests/integration/test_consumergroup_controller.py
@@ -6,7 +6,7 @@ from esque.resources.consumergroup import ConsumerGroup
 
 @pytest.mark.integration
 def test_get_consumer_group(partly_read_consumer_group: str, consumergroup_controller: ConsumerGroupController):
-    instance = consumergroup_controller.get_consumergroup(partly_read_consumer_group)
+    instance = consumergroup_controller.get_consumer_group(partly_read_consumer_group)
     assert isinstance(instance, ConsumerGroup)
 
 
@@ -14,3 +14,20 @@ def test_get_consumer_group(partly_read_consumer_group: str, consumergroup_contr
 def test_list_consumer_groups(partly_read_consumer_group: str, consumergroup_controller: ConsumerGroupController):
     groups = consumergroup_controller.list_consumer_groups()
     assert partly_read_consumer_group in groups
+
+
+def test_delete_consumer_groups(partly_read_consumer_group: str, consumergroup_controller: ConsumerGroupController):
+    groups_before_deletion = consumergroup_controller.list_consumer_groups()
+    assert partly_read_consumer_group in groups_before_deletion
+    consumergroup_controller.delete_consumer_groups(consumer_ids=[partly_read_consumer_group])
+    groups_after_deletion = consumergroup_controller.list_consumer_groups()
+    assert partly_read_consumer_group not in groups_after_deletion
+
+
+def test_delete_nonexistent_consumer_groups(
+    partly_read_consumer_group: str, consumergroup_controller: ConsumerGroupController
+):
+    groups_before = consumergroup_controller.list_consumer_groups()
+    consumergroup_controller.delete_consumer_groups(consumer_ids=["definitely_nonexistent"])
+    groups_after = consumergroup_controller.list_consumer_groups()
+    assert groups_before == groups_after
