bbbf0c251c31e61bdb2eda12128eda0777d31be7
Jannik Hoffjann <hi@hfjn.de>
2021-04-15 11:55:20 +0200

    Fix config path

--- docker_binding.sh
+++ docker_binding.sh
@@ -3,8 +3,8 @@
 esque(){
   docker run \
     --mount type=bind,source="$(pwd)",target=/home/esque/work \
-    --mount type=bind,source="${HOME}/.esque/config.yaml",target=/home/esque/work/config.yaml \
-    -e ESQUE_CONF_PATH=/home/esque/work/config.yaml \
+    --mount type=bind,source="${HOME}/.esque/config.yaml",target=/home/esque/config.yaml \
+    -e ESQUE_CONF_PATH=/home/esque/config.yaml \
     -e PYTHONPATH=/esque \
     esque "${@}"
 }
