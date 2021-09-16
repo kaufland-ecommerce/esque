2fb9c3b3e90897a1fb68a4b3735012f07f5f8142
Swen <swen.wenzel@real-digital.de>
2021-09-16 11:03:46 +0200

    add test for get_offset

--- tests/integration/commands/test_get.py
+++ tests/integration/commands/test_get.py
@@ -1,4 +1,5 @@
 import json
+import math
 import random
 from string import ascii_letters
 from typing import Callable, Tuple
@@ -6,9 +7,9 @@ from typing import Callable, Tuple
 import confluent_kafka
 import pytest
 from click.testing import CliRunner
-from confluent_kafka import Producer
+from confluent_kafka import OFFSET_END, Producer
 
-from esque.cli.commands import get_brokers, get_consumergroups, get_timestamp, get_topics
+from esque.cli.commands import get_brokers, get_consumergroups, get_offset, get_timestamp, get_topics
 from esque.controller.topic_controller import TopicController
 from esque.resources.topic import Topic
 from tests.conftest import parameterized_output_formats
@@ -127,4 +128,47 @@ def test_get_timestamps_with_output_format(
                 expected_offset = 4
             else:
                 expected_offset = 9
-            assert expected_offset == offset_found
+            assert offset_found == expected_offset
+
+
+@pytest.mark.integration
+@parameterized_output_formats
+@pytest.mark.parametrize(
+    argnames="timestamp", argvalues=[1, 45, 105], ids=["ts_at_start", "ts_before_mid", "ts_after_end"]
+)
+def test_get_offset_with_output_format(
+    non_interactive_cli_runner: CliRunner,
+    producer: Producer,
+    target_topic: Tuple[str, int],
+    output_format: str,
+    loader: Callable,
+    timestamp: int,
+):
+    topic_name, partitions = target_topic
+    partition_center = 5
+
+    for target_partition in range(partitions):
+        for msg_offset in range(10):
+            # make sure we don't have any message after offset 'partition_center' for the 10th partition
+            # so we can test the case where one partition's timestamp is after the last available message
+            if target_partition == 9 and msg_offset >= partition_center:
+                continue
+
+            producer.produce(topic_name, value=b"", timestamp=1 + 10 * msg_offset, partition=target_partition)
+    producer.flush()
+
+    result = non_interactive_cli_runner.invoke(get_offset, [topic_name, str(timestamp), "-o", output_format])
+    result_data = loader(result.output)
+    assert len(result_data) == partitions
+
+    for target_partition in range(partitions):
+        if timestamp > 101 or (target_partition == 9 and timestamp > 41):
+            expected_offset = OFFSET_END
+            expected_timestamp = None
+        else:
+            expected_offset = math.ceil((timestamp - 1) / 10)
+            expected_timestamp = 1 + 10 * expected_offset
+
+        assert result_data[target_partition]["offset"] == expected_offset
+
+        assert result_data[target_partition]["timestamp_ms"] == expected_timestamp
