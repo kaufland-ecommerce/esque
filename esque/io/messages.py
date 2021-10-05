70317a591d5d02da8e0f4bd81711fd4c130dd410
Swen <swen.wenzel@real-digital.de>
2021-10-05 09:09:03 +0200

    add default values to message data classes

--- esque/io/messages.py
+++ esque/io/messages.py
@@ -2,7 +2,7 @@ import dataclasses
 import datetime
 from typing import Any, List, NamedTuple, Optional
 
-from esque.io.data_types import DataType
+from esque.io.data_types import DataType, NoData
 
 
 class MessageHeader(NamedTuple):
@@ -15,21 +15,25 @@ class Data(NamedTuple):
     data_type: DataType
 
 
+def now_utc() -> datetime.datetime:
+    return datetime.datetime.now(tz=datetime.timezone.utc)
+
+
 @dataclasses.dataclass
 class Message:
-    key: Data
-    value: Data
-    partition: int
-    offset: int
-    timestamp: datetime.datetime
-    headers: List[MessageHeader]
+    key: Data = Data(None, data_type=NoData())
+    value: Data = Data(None, data_type=NoData())
+    partition: int = -1
+    offset: int = -1
+    timestamp: datetime.datetime = dataclasses.field(default_factory=now_utc)
+    headers: List[MessageHeader] = dataclasses.field(default_factory=list)
 
 
 @dataclasses.dataclass
 class BinaryMessage:
-    key: Optional[bytes]
-    value: Optional[bytes]
-    partition: int
-    offset: int
-    timestamp: datetime.datetime
-    headers: List[MessageHeader]
+    key: Optional[bytes] = None
+    value: Optional[bytes] = None
+    partition: int = -1
+    offset: int = -1
+    timestamp: datetime.datetime = dataclasses.field(default_factory=now_utc)
+    headers: List[MessageHeader] = dataclasses.field(default_factory=list)
